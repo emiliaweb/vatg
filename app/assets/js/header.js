0d1769df45bd767fa193b394b6f070654d94b5b2
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-02-02 16:48:37 +0600

    Darken background when menu is active

--- app/assets/js/header.js
+++ app/assets/js/header.js
@@ -2,12 +2,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const openMenu = document.querySelector('[data-menu-icon=open]');
     const closeMenu = document.querySelector('[data-menu-icon=close]');
     const dropdown = document.querySelector('[data-menu-dropdown]');
+    const overlay = document.querySelector('[data-overlay]');
 
     openMenu.addEventListener('click', () => {
+        overlay.classList.add('header-overlay--active');
         dropdown.classList.add('header-menu-dropdown--active');
     });
 
     closeMenu.addEventListener('click', () => {
+        overlay.classList.remove('header-overlay--active');
+        dropdown.classList.remove('header-menu-dropdown--active');
+    });
+
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('header-overlay--active');
         dropdown.classList.remove('header-menu-dropdown--active');
     });
 })
